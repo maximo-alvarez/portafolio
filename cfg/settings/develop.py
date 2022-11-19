2160e3c974b4437bf25533cd99291fff591214d1
maximo-alvarez <hola@maximoalvarez.com>
2022-11-19 14:28:04 -0500

    mejoras para producción

--- cfg/settings/develop.py
+++ cfg/settings/develop.py
@@ -10,26 +10,26 @@ ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=django,public'
+        },
+        'NAME': 'portafolio',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+    },
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'OPTIONS': {
-#             'options': '-c search_path=django,public'
-#         },
-#         'NAME': 'portafolio',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#
-#     },
-# }
 
 SOCIAL_AUTH_GITHUB_KEY = ''
 SOCIAL_AUTH_GITHUB_SECRET = ''
+
+STATIC_ROOT =  '/var/www/portafolio/static'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
