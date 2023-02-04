f844d7c7b38ede5f7e7b73d33c054a750236a94f
Máximo Álvarez <83915744+maximo-alvarez@users.noreply.github.com>
2023-02-04 17:01:39 -0500

    Update develop.py

--- cfg/settings/develop.py
+++ cfg/settings/develop.py
@@ -11,19 +11,26 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'OPTIONS': {
+#            'options': '-c search_path=django,public'
+#        },
+#        'NAME': 'portafolio',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#
+#    },
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'OPTIONS': {
-            'options': '-c search_path=django,public'
-        },
-        'NAME': 'portafolio',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 SOCIAL_AUTH_GITHUB_KEY = ''
